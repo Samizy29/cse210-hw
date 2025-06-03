c89813481b813b0792b9179078e07f4ef78bb53e
Samizy29 <onisamuel73@gmail.com>
2025-06-03 09:24:59 +0100

    Assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment ma = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(ma.GetSummary());
+        Console.WriteLine(ma.GetHomeworkList());
+
+        WritingAssignment wa = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(wa.GetSummary());
+        Console.WriteLine(wa.GetWritingInformation());
     }
 }
